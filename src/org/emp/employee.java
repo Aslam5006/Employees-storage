3ec8fb4de829de15544ed6a540ee589f93f12207
Aslam5006 <aslam5006@gmail.com>
2021-03-25 13:45:37 +0530

    Employee details

---
+++ src/org/emp/employee.java
@@ -0,0 +1,36 @@
+package org.emp;
+
+public class employee {
+public void employeeId() {
+	System.out.println("employee id is 0101");
+		
+}
+public void employeeName() {
+	System.out.println("employee name is ajith");
+}
+public void employeeDob() {
+	System.out.println("Dob is jan26th 2021");
+	
+}
+public void employeePhone() {
+	System.out.println("phone no is 9876543210");
+
+}
+private void employeeEmail() {
+	System.out.println("email is ajith@gmail.com");
+
+}
+public void employeeAddress() {
+	System.out.println("Greens Technology, Perumbakkam");
+
+}
+public static void main(String[] args) {
+	employee o = new employee();
+	o.employeeId();
+	o.employeeName();
+	o.employeeDob();
+	o.employeePhone();
+	o.employeeEmail();
+	o.employeeAddress();
+}
+}
